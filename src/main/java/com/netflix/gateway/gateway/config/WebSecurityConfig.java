ae73204718350cf64c45e77ead23fc89adc868b4
Flávio Oliveira <flaoliveira@uolinc.com>
2019-11-05 16:27:05 -0300

    liberando healthcheck

--- src/main/java/com/netflix/gateway/gateway/config/WebSecurityConfig.java
+++ src/main/java/com/netflix/gateway/gateway/config/WebSecurityConfig.java
@@ -26,6 +26,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure (HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(
+                        "/actuator/**",
+                        "/h2-console/**"
+                ).permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()),

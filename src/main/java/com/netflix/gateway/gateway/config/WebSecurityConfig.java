2045bea3360fa266b8c0e7ecfc8f3fdd0b7867d6
Flávio Oliveira <flaoliveira@uolinc.com>
2019-11-10 20:05:42 -0300

    liberando url post user sem autenticação

--- src/main/java/com/netflix/gateway/gateway/config/WebSecurityConfig.java
+++ src/main/java/com/netflix/gateway/gateway/config/WebSecurityConfig.java
@@ -25,7 +25,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure (HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf().disable().authorizeRequests()
-                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .antMatchers(HttpMethod.POST, "/login", "/users").permitAll()
                 .antMatchers(
                         "**/actuator/**",
                         "**/h2-console/**"
